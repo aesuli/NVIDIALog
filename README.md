45a3069dc3859b45a11100b0e5e6f85f0bc6c05e
Andrea Esuli <andrea@esuli.it>
2024-05-10 15:53:05 +0200

    argparse

--- README.md
+++ README.md
@@ -10,14 +10,22 @@ The script logs every minute the processes using the GPUs on the system.
 The format of each row is:
 
 ```text
-time, gpu_id, gpu_utilization_precent, gpu_used_memory, process_pid, process_owner, command_line
+time, interval_duration_secs, gpu_id, gpu_utilization_precent, gpu_used_memory, process_pid, process_owner, command_line
 ```
 e.g.:
 ```text
-1715344059,0,100.0,17936.0,130204,esuli,python train.py openwebtext
-1715344120,0,100.0,17936.0,130204,esuli,python train.py openwebtext
+1715344059,60,0,100,17936,130204,esuli,NOT_LOGGED
+1715344120,60,0,100,17936,130204,esuli,NOT_LOGGED
 ```
 
 If multiple process are running at the same moments, multiple lines will be printed.
 If no process is using a GPU, nothing will be printed.
 Utilization percentage is relative to the whole GPU, not to the single process using it.
+
+## Arguments
+
+Command line is logged only if enabled using the `--log_cmdline` argument.
+
+The `--interval` sets the logging interval in seconds.
+
+By default, the log is printed on the standard output, use the `--output_file` to write to a file.
--- nvidialog.py
+++ nvidialog.py
@@ -1,3 +1,4 @@
+import argparse
 import csv
 import pwd
 import sys
@@ -28,17 +29,25 @@ def cmdline(pid):
 
 
 if __name__ == '__main__':
-    interval = 60
-    tocsv = csv.writer(sys.stdout)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--interval', type=int, default=60, help='Interval log in seconds')
+    parser.add_argument('--log_cmdline', action='store_true', help='Enable command-line logging')
+    parser.add_argument('--output_file', type=argparse.FileType('wt', encoding='utf-8'), default=sys.stdout,
+                        help='Output file, default is standard output')
+    args = parser.parse_args()
 
+    interval = args.interval
+    log_cmdline = args.log_cmdline
+    output_file = args.output_file
+
+    to_csv = csv.writer(sys.stdout)
     while True:
         gpus = list(nvsmi.get_gpus())
         gpus = {gpu.id: gpu for gpu in gpus}
         processes = nvsmi.get_gpu_processes()
         now = time.time()
         for process in processes:
-            tocsv.writerow(
-                [int(now), process.gpu_id, gpus[process.gpu_id].gpu_util, process.used_memory, process.pid,
-                 owner(process.pid),
-                 cmdline(process.pid)])
+            to_csv.writerow(
+                [int(now), interval, process.gpu_id, int(gpus[process.gpu_id].gpu_util), int(process.used_memory),
+                 process.pid, owner(process.pid), cmdline(process.pid) if log_cmdline else "NOT_LOGGED"])
         time.sleep(interval)
