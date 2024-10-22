a96888302504837365f8c8b695e6fbbff909f659
Andrea Esuli <andrea@esuli.it>
2024-10-22 22:34:53 +0200

    now command

--- nvidialog-now.py
+++ nvidialog-now.py
@@ -1,5 +1,5 @@
 import nvsmi
-import tabulate
+import pandas as pd
 
 from nvidialog import cmdline, owner
 
@@ -7,7 +7,8 @@ if __name__ == '__main__':
     gpus = list(nvsmi.get_gpus())
     gpus = {gpu.id: gpu for gpu in gpus}
     processes = nvsmi.get_gpu_processes()
-    data = [['GPU', 'Memory', 'PID', 'User', 'Command']]
+    data = []
     for process in processes:
-        data.append([process.gpu_id, int(process.used_memory), process.pid, owner(process.pid), cmdline(process.pid)])
-    print(tabulate.tabulate(data))
+        data.append([process.gpu_id, process.used_memory, process.pid, owner(process.pid), cmdline(process.pid)])
+    df = pd.DataFrame(data, columns=['GPU', 'Memory', 'PID', 'User', 'Command'])
+    print(df)
